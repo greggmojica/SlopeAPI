5e3ef56502a1daf3bfdcd33ae87907e190911663
Gregg Mojica <greggmojica@Macs-MBP-2.home>
2015-06-04 10:24:28 -0400

    Added New Root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :items
 
-  root :to => "index#api"
-  
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
